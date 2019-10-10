1a39187f814f41f3e81b29e372a2a965e8a56f2f
fran <fran@debianfran>
2019-10-10 09:18:51 +0200

    CAMBIOS

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-theme-blog`,
+      resolve: `gatsby-source-filesystem`,
       options: {
         name: `markdown-pages`,
         path: `${__dirname}/src/markdown-pages`,
